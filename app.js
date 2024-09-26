0fe95d0fc5a3d29bc65ad07d3b4a304c4de8acf1
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-09-26 14:36:24 +0100

    setting variables, calling methods, using v-if v-on v-show

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+const app = Vue.createApp({
+    // data, functions, lifecycle hooks etc.
+    // template: '<h1>Hi there2</h1>'
+
+    data() {
+        return {
+            title: 'Wuthering heights',
+            author: 'Joshua',
+            age: 20,
+            showBooks: true,
+        }
+    },
+    methods: {
+        changeTitle() {
+            this.title = "Wuthering heights pt 2"
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    },
+})
+
+app.mount('#app')
+// creates the app instance and mounts it to the element with id app
